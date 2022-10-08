af638fcb1e9c451aec03d1749846ee2730570b12
Drew Peters <megamutha@gmail.com>
2022-10-08 10:50:35 -0400

    @33, 3.08, should have 6 pass but dont

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,16 @@
-
+import heartFilled from "../../svgs/heartFilled.svg"
+import heartOutlined from "../../svgs/heartOutlined.svg"
+src/svgs/heartFilled.svg
 const Card = ({name, phone, email, image, favoured}) => {
     return <div className="card">
         <div className="card-header">
             <img src={image.url} alt={image.alt} className="card-img"/>
+            {/* in the card-header we use a ternary expression to show the right heart svg */}
+            <button className="heart">
+                {favoured ? (
+                    <img src={heartFilled} alt="filled heart" />
+                ) : <img src={heartOutlined} alt="outlined heart" />}
+            </button>
         </div>
         <div className="card-content">
             <h3>{name}</h3>
--- src/components/Card/__test__/Card.test.js
+++ src/components/Card/__test__/Card.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import Card from "../Card"
+import userEvents from "@testing-library/user-event";
 
 //set up a var with default props for our Card components:
 const cardProps = {
@@ -27,6 +28,7 @@ describe("Card", () => {
             })
         ).toBeInTheDocument();
     });
+    
     test("should show phone number for cat", () => {
         render(
             <Card
@@ -39,6 +41,7 @@ describe("Card", () => {
             // we can get this by role but we're trying getByText()
             screen.getByText(/555-555-5555/i)).toBeInTheDocument();
     });
+    
     test("should show email for cat", () => {
         render(
             <Card
@@ -48,6 +51,7 @@ describe("Card", () => {
         expect(
             screen.getByText(/drew@gmail.com/i)).toBeInTheDocument();
     });
+    
     test("should get the picture of cat", () => {
         render(
             <Card
@@ -57,5 +61,34 @@ describe("Card", () => {
         //get the element by the alt text, then check that the element is the url in our Card component
         expect(screen.getByAltText(/cute cat/i).src).toBe(cardProps.image.url);
     });
+
+    test("should show outlined heart", () => {
+        render(<Card {...cardProps} />);
+        expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+        expect(screen.getAllByAltText(/outlined heart/i)).toBeInTheDocument();
+    });
+
+    test("should show filled heart", () => {
+        //use default props except overwrite the 'favoured' prop:
+        render(<Card {...cardProps} favoured = {true} />);
+        expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+        expect(screen.getAllByAltText(/filled heart/i)).toBeInTheDocument();        
+    });
+
+    test("Should toggle heart status", () => {
+        render(<Card {...cardProps} />);
+        //first click should fill heart
+        userEvents.click(screen.getByRole("button"));
+        expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+        expect(screen.getAllByAltText(/filled heart/i)).toBeInTheDocument();
+        
+        //2nd click should clear the heart
+        userEvents.click(screen.getByRole("button"));
+        expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+        expect(screen.getAllByAltText(/outlined heart/i)).toBeInTheDocument();
+
+
+    });
+
 });//describe 'Card'
 
