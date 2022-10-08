3428b94f0b46ea22faece789d82964311eef89b5
Drew Peters <megamutha@gmail.com>
2022-10-08 11:32:19 -0400

    @34, onClick function mystery

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import heartFilled from "../../svgs/heartFilled.svg"
 import heartOutlined from "../../svgs/heartOutlined.svg"
 // src/svgs/heartFilled.svg
 const Card = ({name, phone, email, image, favoured}) => {
+    //setting up a localState w/ initial value from our props that came in with anon func
+    const[isFavoured, setIsFavoured] = useState(favoured);
+    const toggleFavoured = () => {
+        setIsFavoured(!isFavoured);
+    };
     return <div className="card">
         <div className="card-header">
             <img src={image.url} alt={image.alt} className="card-img"/>
             {/* in the card-header we use a ternary expression to show the right heart svg */}
-            <button className="heart">
-                {favoured ? (
+            <button className="heart" onClick={toggleFavoured}>
+                {isFavoured ? (
                     <img src={heartFilled} alt="filled heart" />
                 ) : <img src={heartOutlined} alt="outlined heart" />}
             </button>
-        </div>
+        </div>  {/* closing the card-header div */}        
         <div className="card-content">
             <h3>{name}</h3>
             <p>{phone}</p>
