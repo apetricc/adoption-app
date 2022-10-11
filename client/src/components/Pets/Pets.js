8f567062b17bef37c3898d351c1c24bf90e6bd3a
andrew.petriccione <andrew.petriccione@metlife.com>
2022-10-10 21:32:24 -0400

    add axios @v44

--- client/src/components/Pets/Pets.js
+++ client/src/components/Pets/Pets.js
@@ -1,14 +1,38 @@
 import "./Pets.css";
 import Filter from "../Filter/Filter";
 import Cards from "../Cards/Cards";
+// import cats from "../../mocks/cats.json"
+import axios from "axios"
+import { useEffect, useState } from "react";
 
-const Pets = () => (
-    <div className="container">
-        <div className="app-container">
-            <Filter />
-            <Cards cats={[]} />
+
+const Pets = () => {
+    //this use state hook will start as an empty array
+    const [cats, setCats] = useState([]);
+
+    //we want to call this function on the initial render of the component
+    const fetchCats = async () => {
+        const response = await axios.get("http://localhost:4000/cats");
+        setCats(response.data);
+    };    
+
+    useEffect(() => {
+        fetchCats()
+    }, []);
+
+    console.log(cats);
+
+    return (
+        <div className="container">
+            <div className="app-container">
+                <Filter />
+                <Cards cats={cats} />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Pets;
+
+//original test function: 
+
